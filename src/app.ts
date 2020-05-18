8f1370bae9693dd97d7fe0bf318676cb3f50c3ca
Darey <horla@Dareys-MacBook-Pro.local>
2020-05-18 10:05:06 +0100

    ddd

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,10 @@
-import express from "express";
-
-const app = express();
-import { ApolloServer, gql } from "apollo-server-express";
+import { ApolloServer } from "apollo-server";
 import db from "./utils/connect";
 import { typeDefs, resolvers } from "./utils/models";
 
 const server = new ApolloServer({ typeDefs, resolvers });
-server.applyMiddleware({ app, path: "/graphql" });
 
-app.get("/", (req, res) => res.send("Ola"));
-app.listen({ port: process.env.PORT }, () => {
+server.listen({ port: process.env.PORT }).then(({ url }) => {
   db.on;
-  console.log("Apollo Server on http://localhost:8000/graphql");
+  console.log(`Server ready at ${url}`);
 });
